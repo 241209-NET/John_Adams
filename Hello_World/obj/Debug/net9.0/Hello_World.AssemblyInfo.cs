eafa3c4b7d6e7fe53715782d2e36933e0e9c9b21
john109695 <64110752+JohnAdams2@users.noreply.github.com>
2024-12-10 10:30:46 -0500

    Config changes

--- Hello_World/obj/Debug/net9.0/Hello_World.AssemblyInfo.cs
+++ Hello_World/obj/Debug/net9.0/Hello_World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cadf2bbb14a477d8e15b9f88fae6fb24cf83aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
