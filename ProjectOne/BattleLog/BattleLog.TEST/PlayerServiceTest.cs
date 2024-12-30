9d2cedbc447ff38e0a172b41007046c95c76c88e
john109695 <64110752+JohnAdams2@users.noreply.github.com>
2024-12-30 16:37:49 -0500

    Added Test project and started testing

---
+++ ProjectOne/BattleLog/BattleLog.TEST/PlayerServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+using AutoMapper;
+using BattleLog.API.DTO;
+using BattleLog.API.Model;
+using BattleLog.API.Repository;
+using BattleLog.API.Service;
+using Moq;
+
+namespace BattleLog.TEST;
+
+public class PlayerServiceTest
+{
+    [Fact]
+    public void TestCreateNewPlayer()
+    {
+        //Arrange
+        Mock<IPlayerRepository> mockRepo = new();
+        // Configure AutoMapper
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>(); // Use your actual mapping profile class
+        });
+        IMapper mapper = config.CreateMapper();
+        PlayerService playerService = new(mockRepo.Object, mapper);
+
+        List<Player> playerList = [
+            new Player{Id = 1, Name = "Bob", AttackPower = 1, Experience = 1, Health = 5},
+            new Player{Id = 2, Name = "Bill", AttackPower = 2, Experience = 1, Health = 5},
+            new Player{Id = 3, Name = "Tim", AttackPower = 4, Experience = 1, Health = 5},
+            new Player{Id = 4, Name = "Jim", AttackPower = 8, Experience = 1, Health = 5},
+            new Player{Id = 5, Name = "Man", AttackPower = 16, Experience = 1, Health = 5}
+        ];
+
+        Player newPlayer = new Player{Id = 1, Name = "Frank", AttackPower = 1, Experience = 0, Health = 5};
+
+        mockRepo.Setup(repo => repo.CreateNewPlayer(It.IsAny<Player>()))
+            .Callback((Player p) => playerList.Add(p))
+            .Returns(newPlayer);
+        
+        PlayerInDTO playerInDTO = new PlayerInDTO{Name = newPlayer.Name, AttackPower = newPlayer.AttackPower, Health = newPlayer.Health};
+        //Act
+        var myPlayer = playerService.CreateNewPlayer(playerInDTO);
+
+        //Assert
+        Assert.Contains(newPlayer, playerList);
+        mockRepo.Verify(x => x.CreateNewPlayer(It.IsAny<Player>()), Times.Once());
+    }
+}
